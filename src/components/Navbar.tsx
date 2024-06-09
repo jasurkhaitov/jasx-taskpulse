c7bea18829e71f32353c9c649f00184121ca2b8a
jasur <fake49735@gmail.com>
2024-06-10 00:14:52 +0500

    modal problem fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,8 +61,6 @@ export default function Navbar() {
 
               <div className='border-[1px] py-[6px] px-[10px] rounded-lg border-black dark:border-white border-opacity-30 duration-100 delay-150 flex items-center gap-2 justify-center w-[200px] m-auto'>
                 <Link className='px-[12px] py-[4px] dark:hover:bg-white hover:bg-black hover:bg-opacity-20 dark:hover:bg-opacity-20 duration-100 delay-150 rounded-lg text-base text-black dark:text-white' to='/login'>Login</Link>
-            
-                <AlertDialogCancel className='m-auto'>Cancel</AlertDialogCancel>
 
                 <Link className='px-[12px] py-[4px] rounded-lg bg-gradient-to-r from-blue-500 to-pink-500 text-base text-white' to='/signup'>Signup</Link>
               </div>
@@ -71,7 +69,7 @@ export default function Navbar() {
           <AlertDialogCancel className='m-auto'>Cancel</AlertDialogCancel>
         </AlertDialogFooter>
       </AlertDialogContent>
-    </AlertDialog>
+          </AlertDialog>
         </div>
       </div>
     </nav>
